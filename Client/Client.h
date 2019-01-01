30045d4db6f6e65e21cc962c4a99a39455659ef9
hechenrui <hechenrui123@gmail.com>
2019-01-01 20:14:23 +0800

    FEA: implement client on() and send()

--- Client/Client.h
+++ Client/Client.h
@@ -7,6 +7,24 @@
 
 #define INI_FILE_NAME "./config.ini"
 
+enum POS {
+    dest_user=0,
+    src_user=4,
+    frame_id=8,
+    option=10,
+    stamp=12,
+    ret=12,
+	data=12,
+    direction=16,
+    longitude=17,
+    latitude=21,
+    altitude=25,
+	cable=29,
+	beam_length=30,
+	snr=31
+};
+
+
 class Client
 {
   public:
@@ -17,11 +35,20 @@ class Client
   private:
 	string m_name;
 	string m_channelIP;
+	unsigned int m_userID;
 	unsigned short m_frameNo;
 	unsigned short m_channelPort;
 	unsigned short m_clientPort;
+	vector<unsigned int> m_peers;
     CUDPInterface m_UDPInterface;
 
 	int readInitFile();
 	int userOn();
+	int userOnWait();
+	int userSend(bool isRet, bool isCnf, unsigned int id);
+	int userSendWaitRet(unsigned int id);
+	int userSendWaitCnf(unsigned int id);
 };
+
+
+
